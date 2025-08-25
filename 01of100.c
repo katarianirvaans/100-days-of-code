df59dd20b20910e93f599676c54860d6a939150e
katarianirvaans <katarianirvaans@gmail.com>
2025-08-25 21:54:27 +0530

    Create 01of100.c

---
+++ 01of100.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+/*Q1: Write a program to input two numbers and display their sum.
+
+
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+int main(){
+
+  int a=3;
+  int b=4;
+  int c=-1;
+  int d=20;
+  int sum1;
+  int sum2;
+
+
+  sum1 = a+b;
+  printf("The sum of a and b is:%d \n" , sum1);
+
+  sum2 = c+d;
+  printf("The sum of c and d is:%d \n" , sum2);
+
+    return 0;
+}
