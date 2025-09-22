c0239fac17b6e26f62c4a9d521520809a472c6bb
katarianirvaans <katarianirvaans@gmail.com>
2025-09-22 15:38:06 +0530

    Create 47of200.c

---
+++ 47of200.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+
+    for ( i = 1; i < 2; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 3; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 4; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 5; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+
+     for ( i = 1; i < 6; i++)
+    {
+        printf("*");
+    }
+
+    printf("\n");
+    
+    
+    
+    return 0;
+}
